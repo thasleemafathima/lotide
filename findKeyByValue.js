54342acfb2f1b4f0c745c3e66542978c6c3a3e60
Thasleema Fathima Puroskhan <thasleemafathima92@gmail.com>
2020-08-20 02:30:09 +0000

    func to find the key using the key value

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+const findKeyByValue = function(obj,val) {
+  for (let allObj in obj) {
+    //console.log(obj[allObj],val)
+    if (obj[allObj] === val) {
+      return allObj;
+    }
+  }//console.log(ans)
+};
+
+
+
+
+
+
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
