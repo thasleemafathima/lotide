ddcb71cdea6fc30e7fc9ff162670c79c6053cf2a
Thasleema Fathima Puroskhan <thasleemafathima92@gmail.com>
2020-08-21 03:38:13 +0000

    function passing a funtion and a object

---
+++ findKey.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+
+const findKey = function(obj,callback) {
+  for (let obji in obj) {
+    if (callback(obj[obji]) === true) {
+      return obji;
+    }
+  }
+  return undefined;
+};
+
+
+const new1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+console.log(assertEqual(new1,"noma"));
