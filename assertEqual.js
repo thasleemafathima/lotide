372f9bd7ebc40870325df66e8bb1306ffb9fa1ad
Thasleema Fathima Puroskhan <thasleemafathima92@gmail.com>
2020-08-23 23:20:02 +0000

    adding module references to our code

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,4 @@
-// FUNCTION IMPLEMENTATION
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -8,6 +8,9 @@ const assertEqual = function(actual, expected) {
 
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+
+
+
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,16 +1,6 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
 
-};
+const assertEqual = require('./assertEqual');
 
-// TEST CODE
-//assertEqual("Lighthouse Labs", "Bootcamp");
-//assertEqual(1, 1);
 
 const head = function(array) {
   if (array === []) {
@@ -19,6 +9,3 @@ const head = function(array) {
     return array[0];
   }
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]));
