908f66a8a8cb8b22def7db9d5f860c66ea61f254
Thasleema Fathima Puroskhan <thasleemafathima92@gmail.com>
2020-08-24 00:09:59 +0000

    assertarrays equal refactoring in lotide

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(array1,array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i in array1) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1,arr2) {
   if (eqArrays(arr1,arr2) === true) {
@@ -19,4 +8,4 @@ const assertArraysEqual = function(arr1,arr2) {
   }
 };
 
-console.log(assertArraysEqual([1,2,3],[1,2,3]));
+module.exports = assertArraysEqual;
