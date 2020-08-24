b0e6e6ec26ca131d48e68d5c9c63898384e35572
Thasleema Fathima Puroskhan <thasleemafathima92@gmail.com>
2020-08-24 01:12:39 +0000

    creating an index object to store all our functions

---
+++ index.js
@@ -0,0 +1,13 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertEqual: assertEqual,
+  eqArrays: eqArrays
+};
