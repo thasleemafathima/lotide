45f03b260db59e462e63cb520eefab28cb9e2249
Thasleema Fathima Puroskhan <thasleemafathima92@gmail.com>
2020-08-24 00:14:42 +0000

    middle refactoring for lotide

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-const eqArrays = function(array1,array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i in array1) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-const assertArraysEqual = function(arr1,arr2) {
-  if (eqArrays(arr1,arr2) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-//console.log(assertArraysEqual([1,2,3],[1,2,3]));
-
 const middle = function(array){
   let middle = [];
   if(array.length < 3){
@@ -34,4 +11,4 @@ const middle = function(array){
 return middle;
 }
 
-console.log(assertArraysEqual(middle([1,2,2,4,5]),[2,2]));
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,4 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+assertArraysEqual(middle([1,2,7,4,5]),[7]);
